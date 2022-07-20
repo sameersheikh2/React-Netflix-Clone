f96d51b2e8e9352de6ced951238c66b2afbf518e
sameersheikh2 <sameersheikh819@gmail.com>
2022-07-20 18:36:31 +0530

    added function to login using existing email and password

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,8 +1,24 @@
-import React from 'react'
-import { Link } from "react-router-dom";
-
+import React, { useState } from 'react'
+import { Link , useNavigate } from "react-router-dom";
+import { UserAuth } from '../context/AuthContext';
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error , setError] = useState('')
+    const { user, logIn } = UserAuth();
+    const navigate = useNavigate();
+  
+    const submitHandler = async (e) => {
+      e.preventDefault();
+      try {
+        await logIn(email, password);
+        navigate("/");
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
   return (
     <>
       <div className="w-full h-screen">
@@ -16,13 +32,20 @@ const Login = () => {
           <div className="max-w-[450px] h-[510px] bg-black/75 text-white mx-auto">
             <div className="max-w-[320px] mx-auto py-16">
               <h1 className="text-3xl font-bold">Sign In</h1>
-              <form className="flex flex-col py-4 w-full">
+              {error ? <p className='bg-red-400 p-2 my-3'>{error}</p> : null}
+              <form onSubmit={submitHandler} className="flex flex-col py-4 w-full">
                 <input
+                onChange={(e)=>{
+                    setEmail(e.target.value)
+                }}
                   className="p-3 my-3 rounded bg-gray-600"
                   type="email"
                   placeholder="Email"
                 />
                 <input
+                onChange={(e)=>{
+                    setPassword(e.target.value)
+                }}
                   className="p-3 my-3 rounded bg-gray-600"
                   type="password"
                   placeholder="Password"
