2e2b9b879dbde27fb86315c89a3d9f0f4020aa1a
sameersheikh2 <sameersheikh819@gmail.com>
2022-07-20 18:19:15 +0530

    added sigup and logout functionality also removed some type error in .env file for KEYS

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth
+import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
 // Import the functions you need from the SDKs you need
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,3 +19,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+// export const db = getFirestore(app)
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,39 @@
 import React from 'react'
+import {Link , useNavigate} from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 
 const Navbar = () => {
+ const {user , logOut} = UserAuth();
+ const navigate = useNavigate()
+
+const logoutHandler =async()=>{
+    try {
+       await logOut()
+       navigate('/')
+    } catch (error) {
+     console.log(error)
+    }
+}
+
     return (
         <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
+            <Link to='/'>
             <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
-            <div>
-                <button className='pr-4 text-white'>Sign In</button>
-                <button className='bg-red-600 px-6 py-2 rounded cursor-pointer ' >Sign Up</button>
-            </div>
+            </Link>
+          {user?.email ?   <div>
+                <Link to='/account'>
+                <button className='pr-4 text-white'>Account</button>
+                </Link>
+                <button onClick={logoutHandler} className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white' >Log out</button>
+            </div> :
+              <div>
+              <Link to='/login'>
+              <button className='pr-4 text-white'>Sign In</button>
+              </Link>
+              <Link to='/signup'>
+              <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white' >Sign Up</button>
+              </Link>
+          </div>}
         </div>
     )
 }
