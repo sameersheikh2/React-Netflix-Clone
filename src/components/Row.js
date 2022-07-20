0b900ba7929a2232acd3b101998df52e663973d1
sameersheikh2 <sameersheikh819@gmail.com>
2022-07-20 11:35:41 +0530

    separate code to show movie in row into Movie.js component and render it into Row.js

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { FaHeart, FaRegHeart } from "react-icons/fa";
+
+import Movie from "./Movie";
 
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([]);
-  const [like, setLike] = useState(false);
 
   useEffect(() => {
     axios.get(fetchURL).then((response) => {
@@ -20,19 +20,7 @@ const Row = ({ title, fetchURL }) => {
       <div className="relative flex items-center">
         <div id={"slider"}>
           {movies.map((item, id) => (
-            <div className="cursor-pointer w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block relative p-2">
-              <img
-                className="w-full h-auto block"
-                src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
-                alt={item?.title}
-              />
-              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-                <p className="white-space-normal text-sm md:text-sm font-bold flex justify-center items-center h-full text-center">
-                  {item?.title}
-                </p>
-                <p>{like ? <FaHeart className="absolute top-4 left-4 text-gra-300"/> : <FaRegHeart className="absolute top-4 left-4 text-gra-300"/>}</p>
-              </div>
-            </div>
+            <Movie key={id} item={item} />
           ))}
         </div>
       </div>
